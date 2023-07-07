253c4cdd183c2221c2cbf5dd4f6e7b8572a0d510
culhamu <mustafa.qatester@gmail.com>
2023-07-07 12:23:05 +0300

    07.07 Mustafa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/culhamu/karate_api.git
+        git push -u origin main
+    }
+}
